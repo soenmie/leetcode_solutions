fbfdacf9d684bc958e470a6ee09cfb89ff21d8ad
sunmian <svnmian@gmail.com>
2013-08-30 14:13:09 +0800

    Pow(x, n) Added
    Signed-off-by: sunmian <svnmian@gmail.com>

---
+++ Pow(x, n)/Solution.java
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public double pow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        if (x == 0.0) {
+            return 0.0;
+        }
+        else if (n < 0) {
+            x = 1.0 / x;
+            n = -n;
+        }
+
+        double y = pow(x, n / 2);
+        
+        if (n % 2 == 0) {	
+            return y * y;
+        }
+        else {
+            return y * y * x;
+        }   
+    }
+}
